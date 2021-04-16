d826a609aa9a7b3c46ae0e126e7f35934a88d0c8
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2021-04-16 09:48:44 +0100

    refactored select dropdown

---
+++ quotes.js
@@ -0,0 +1,72 @@
+const aries = ['Aries get things done their way. And they don’t always explain themselves to others. Explanations take time and that’s not Aries sign up for.', 
+'Tell an Aries they can’t, and they will prove you wrong.',
+'Aries don’t have dreams. They call them plans.',
+'Aries don’t get scared when meeting a challenge. They get excited. '];
+
+const taurus = ['Taurus are doing more than you know, on things you don’t know about.',
+'Taurus remembers everything you do and don’t do.',
+'Treat Taurus like an option, and they will cross you out completely.',
+`Two things Taurus want: Be successful + Not leaving bed`];
+
+const gemini = ['Geminis will give you all the attention until you give them a reason not to.',
+'A Gemini can miss you and hate you at the same time.',
+'Gemini is indecisive. But once they make up their mind,n othing will stop them.'];
+
+const cancer = ['Cancerians shower their friends with parental care.',
+'Cancers know they should put themselves first but still try to make sure everyone is happy.',
+'Don’t try to ghost on Cancers. They will hunt you down if they care about you enough.',
+'Cancers are motivated by people they love and hate.'];
+
+const leo = ['Leos are sometimes offended by their own attitude.',
+'Leos have trouble finding the fine line between being confident and arrogant.',
+'Leos have no time for haters.',
+'Beware when you turn to Leos for advice. They will punch you in the face with cold, harsh truths.',
+'Don’t underestimate a soft Leo. You don’t want to see the other side.'];
+
+const virgo = ['Virgos know how to set aside feelings and get things done.',
+'Virgos are messy themselves but don’t like others being messy.',
+'They may forgive, but never forget.',
+'Virgos give advice that they don’t follow.'];
+
+const libra = ['Libra is the best friend. They always listen and never judge.',
+'Libras don’t “lose it”. They keep their cool even in chaos.',
+'Libra have their opinion. But they keep it to themselves.',
+'Libras either care too much or don’t care at all.'];
+
+const scorpio = ['If Scorpios like you, they study you, in detail.',
+'Scorpios are intense and chill at the same time.',
+'Scorpios may want you, but they don’t need you.',
+'Stop asking Scorpios for their trust and start proving that you deserve it.'];
+
+const sagittarius = ['Sagittarius is the life of a party.',
+'Don’t try to control Sagittarius. It pushes them away.',
+'Sagittarius has no problem throwing the ugly truth at your face.',
+'S.A.G.I.T.T.A.R.I.U.S: Spontaneous. Adventurous. Genuine. Intelligent. Tireless. Tactful. Alluring. Robust. Insightful. Upbeat. Sharp-witted'];
+
+const capricorn = ['Capricorn’s heart is like a buried treasure that can only be found with patience.',
+'Capricorn: Your best chance to survive a zombie apocalypse',
+'Everything has a solution, and Capricorn knows where to find one.',
+'If you are offended by Capricorn’s words, know that they only let out 1%.'];
+
+const aquarius = ['Don’t try to label Aquarius as someone else’s. They belong to themselves 100%.',
+'Aquarius stands out without trying.',
+'Aquarius will make you feel comfortable in your own skin.',
+'Aquarius – the ambassador of open-mindedness'];
+
+const pisces = ['P.I.S.C.E.S: Pleasant. Insecure. Sensitive. Caring. Empathetic. Sophisticated',
+'Pisces kiss like it’s the last kiss they are going to give.',
+'Pisces feels lonely even in a crowd, especially a wrong one.',
+'Don’t play games with Pisces. They can tell before you even try.'];
+
+export const userHoroscope = { aries,
+        taurus, 
+        gemini, 
+        cancer, 
+        leo, 
+        virgo, 
+        libra, 
+        scorpio, 
+        sagittarius, 
+        capricorn, 
+        aquarius, 
+        pisces };
